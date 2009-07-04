0fb9d9672d594b236e5b3dfb3be77a245eef2a38
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-05 00:25:55 +0200

    catch broken encodings (still unclear how to do this correctly), catch parse errors from ripper2ruby (happens on Ruby 1.9 incompatible code)

--- lib/i18n/index/file.rb
+++ lib/i18n/index/file.rb
@@ -9,18 +9,27 @@ module I18n
     end
     
   	class File
-  	  attr_accessor :path
+  	  attr_accessor :filename
 
-  	  def initialize(path)
-  	    self.path = path
+  	  def initialize(filename)
+  	    self.filename = filename
 	    end
 
 	    def source
-	      @source ||= Index.filter(::File.read(path), path)
+	      @source ||= begin
+	        source = ::File.read(filename) # TODO srsly ... how am i supposed to open a file in ruby 1.9?
+	        source = ::File.open(filename, 'r:iso-8859-1:utf-8') { |f| f.read } unless source.valid_encoding?
+	        source
+        end
       end
 	    
 	    def ruby
-	      @ruby ||= parser.parse
+	      @ruby ||= begin
+	        parser.parse
+	      rescue Ripper::RubyBuilder::ParseError => e
+	        puts "\nWARNING Ruby 1.9 incompatible syntax in: " + e.message.gsub(Dir.pwd, '') + ". Can not index file."
+	        Ruby::Program.new(source, filename)
+        end
       end
       
       def calls
@@ -33,13 +42,13 @@ module I18n
       end
 
 	    def save
-        ::File.open(path, 'w+') { |f| f.write(source) } # TODO need to modify/write unfiltered source for ERB
+        ::File.open(filename, 'w+') { |f| f.write(source) } # TODO need to modify/write unfiltered source for ERB
       end
       
       protected
 
         def parser
-          @parser ||= Index.parser.new(source, path)
+          @parser ||= Index.parser.new(source, filename)
         end
 	  end
   end
